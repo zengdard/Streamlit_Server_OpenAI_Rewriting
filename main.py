ce0f455b0b501e34d905fb8691ecb211d7916cea
Nielzaclord <61885397+Nielzaclord@users.noreply.github.com>
2023-08-04 16:07:59 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+import snscrape.modules.twitter as sntwitter
+import openai
+import streamlit as st
+
+# Votre clé d'API pour OpenAI
+openai.api_key = 'your-openai-api-key'
+
+# Widget pour saisir l'ID du tweet
+tweet_id = st.text_input('Enter Tweet ID')
+
+if tweet_id:
+    # Récupération des commentaires
+    comments = list(sntwitter.TwitterSearchScraper(f'to:{tweet_id}').get_items())
+
+    # Création d'une liste de commentaires
+    comments_list = [comment.content for comment in comments]
+
+    # Construction du prompt pour GPT-3.5
+    prompt = "Given the following comments on a tweet, identify which ones provide valuable context and which ones are irrelevant:\n"
+    for i, comment in enumerate(comments_list):
+        prompt += f"{i+1}: {comment}\n"
+
+    # Utilisation de GPT-3.5 pour analyser les commentaires
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        temperature=0.5,
+        max_tokens=100
+    )
+
+    # Affichage de l'analyse
+    st.write(f"Analysis: {response.choices[0].text.strip()}")
