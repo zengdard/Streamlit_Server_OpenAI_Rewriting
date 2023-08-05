10820c9f3f1b35bf5a0cd1c5f501b4e43b9d51be
Nielzaclord <61885397+Nielzaclord@users.noreply.github.com>
2023-08-05 12:17:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -28,14 +28,21 @@ clients = tweepy.Client(bearer_token)
 # Additional fields can be retrieved using the tweet_fields parameter
 # Widget pour saisir l'ID du tweet
 tweet_ids = st.text_input('Enter Tweet ID')
-
-if tweet_ids:
+name = st.text_input('Enter Tweet ID')
+if tweet_ids and name :
+        
+    replies=[]
+    for tweet in tweepy.Cursor(api.search,q='to:'+name, result_type='recent', timeout=999999).items(1000):
+        if hasattr(tweet, 'in_reply_to_status_id_str'):
+            if (tweet.in_reply_to_status_id_str==tweet_id):
+                replies.append(tweet)
     # Récupération des commentaires
-    comments = api.get_tweets(tweet_ids, tweet_fields=["in_reply_to_user_id", "referenced_tweets", "context_annotations", "edit_history_tweet_ids"])
+    #comments = api.get_tweets(tweet_ids, tweet_fields=["in_reply_to_user_id", "referenced_tweets", "context_annotations", "edit_history_tweet_ids"])
 
     # Création d'une liste de commentaires
-    comments_list = comments.data
-    print(comments_list)
+    
+    print(replies)
+    comments_list = replies
     # Construction du prompt pour GPT-3.5
     prompt = "Given the following comments on a tweet, identify which ones provide valuable context and which ones are irrelevant:\n"
     for i, comment in enumerate(comments_list):
