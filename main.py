f1301942116b25582342ea8ba00e139c79d1a7a1
Nielzaclord <61885397+Nielzaclord@users.noreply.github.com>
2023-08-07 13:15:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,47 +1,37 @@
 import openai
 import streamlit as st
-import tweepy
 
 
 # Votre clé d'API pour OpenAI
 openai.api_key = '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'
 
-consumer_key = 'AknQg4BbBgDnhJ8W8O6p8D7tS'
-consumer_secret = 'AhjaPSZBmbGfaj4CmmFZjjwv4oypS7dmT2MNVGJfPBrL3h9dtg'
-access_token = '1526862349944205313-pGiuUmUFoMhS04MFLRIbq3JKZq75w3'
-access_token_secret = 'rwhiGeTpF16oWD59mD7nv4RVFnLrv3jXlT993elPqPSTc'
+def rewrite_text_objectively(text):
+    try:
+        response = openai.Completion.create(
+            engine="davinci",
+            prompt=f"Réécrivez ce texte de manière objective : \"{text}\"",
+            max_tokens=150
+        )
+        return response.choices[0].text.strip()
+    except Exception as e:
+        st.write("Erreur lors de la réécriture:", str(e))
+        return text
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
+st.title("Réécriture de texte de manière objective")
 
-# This endpoint/method returns a variety of information about the Tweet(s)
-# specified by the requested ID or list of IDs
-name = st.text_input('name')
-tweet_id = st.text_input('Enter Tweet ID')
-if tweet_id and name :
-    # Récupération des commentaires
-    replies=[]
-    for tweet in tweepy.Cursor(api.search_tweets,q='to:'+name, result_type='recent').items(1000):
-        if hasattr(tweet, 'in_reply_to_status_id_str'):
-            if (tweet.in_reply_to_status_id_str==tweet_id):
-                replies.append(tweet)
-    
-    print(replies)
-    comments_list = replies
-    # Construction du prompt pour GPT-3.5
-    prompt = "Given the following comments on a tweet, identify which ones provide valuable context and which ones are irrelevant:\n"
-    for i, comment in enumerate(comments_list):
-        prompt += f"{i+1}: {comment}\n"
+# Entrée utilisateur
+user_input = st.text_area("Entrez votre texte (max 200 mots) :")
 
-    # Utilisation de GPT-3.5 pour analyser les commentaires
-    response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=prompt,
-        temperature=0.5,
-        max_tokens=100
-    )
+# Vérifier la longueur du texte
+word_count = len(user_input.split())
 
-    # Affichage de l'analyse
-    st.write(f"Analysis: {response.choices[0].text.strip()}")
-
+if word_count > 200:
+    st.write(f"Votre texte contient {word_count} mots. Veuillez réduire votre texte à 200 mots ou moins.")
+else:
+    if st.button("Réécrire"):
+        if user_input:
+            rewritten_text = rewrite_text_objectively(user_input)
+            st.subheader("Texte réécrit :")
+            st.write(rewritten_text)
+        else:
+            st.write("Veuillez entrer un texte à réécrire.")
